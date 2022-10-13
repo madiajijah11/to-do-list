4410879de0ff24489ced141ce49154e612d26d9e
MexL <madiajijah7@gmail.com>
2022-10-14 00:14:44 +0800

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+	return (
+		<div className="header">
+			<div data-cy="header-background" className="container">
+				<h2 data-cy="header-title" className="dashboard-title">
+					TO DO LIST APP
+				</h2>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
