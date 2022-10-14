28ca26b43d491dca6fb6b33a115e93fb68312c9e
MexL <madiajijah7@gmail.com>
2022-10-14 15:15:40 +0800

    perbaikan sedikit

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -20,21 +20,34 @@ const Content = () => {
 		fetchTodos();
 	}, []);
 
+	const tambahActivity = async () => {
+		const response = await fetch("https://todo.api.devcode.gethired.id/activity-groups", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await response.json();
+		setTodos(data);
+	};
+
 	return (
 		<div className="container">
 			<div className="header-content">
 				<h1>Activity</h1>
-				<button data-cy="activity-add-button" className="btnTambah">
+				<button
+					data-cy="activity-add-button"
+					onClick={tambahActivity}
+					className="btnTambah">
 					<i className="bi-plus" />
 					Tambah
 				</button>
 			</div>
-
-			{todos.data ? (
-				todos.data.map((todo) => (
-					<Row xs={1} md={4} className="g-4">
+			<Row xs={1} md={4} className="g-4 pb-5">
+				{todos.data ? (
+					todos.data.map((todo, index) => (
 						<Col key={`todos_${todo.id}`}>
-							<Card className="shadow-sm">
+							<Card data-cy={`ativity-item-${index + 1}`} className="shadow-sm">
 								<Card.Body>
 									<Card.Title>{todo.title}</Card.Title>
 									<Card.Text>
@@ -43,20 +56,20 @@ const Content = () => {
 								</Card.Body>
 							</Card>
 						</Col>
-					</Row>
-				))
-			) : (
-				<Card className="shadow-sm">
-					<Card.Body>
-						<button className="btn btn-info">
-							<i className="bi-plus" />
-						</button>
-					</Card.Body>
-					<Card.Footer>
-						<p>Buat activity pertamamu</p>
-					</Card.Footer>
-				</Card>
-			)}
+					))
+				) : (
+					<Card className="shadow-sm">
+						<Card.Body>
+							<button className="btn btn-info">
+								<i className="bi-plus" />
+							</button>
+						</Card.Body>
+						<Card.Footer>
+							<p>Buat activity pertamamu</p>
+						</Card.Footer>
+					</Card>
+				)}
+			</Row>
 		</div>
 	);
 };
